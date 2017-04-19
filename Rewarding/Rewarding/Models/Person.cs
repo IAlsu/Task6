4affec2c59f65cece6201c628efb468619e9ccba
Alsu Islamova <Alsu_Islamova@epam.com>
2017-04-20 00:06:01 +0600

    Add task 6

---
+++ Rewarding/Rewarding/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rewarding.Models
+{
+    [Table("Persons")]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+        public int Age { get; set; }
+
+        public virtual ICollection<Reward> Rewards { get; set; }
+        public Person()
+        {
+            Rewards = new List<Reward>();
+        }
+
+        public int? PhotoId { get; set; }
+        [ForeignKey("PhotoId")]
+        public Image Photo { get; set; }
+    }
+}
+
---
+++ Rewarding/Rewarding/Models/Reward.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Rewarding.Models
+{
+    public class Reward
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Person> Persons { get; set; }
+        public Reward()
+        {
+            Persons = new List<Person>();
+        }
+
+        public int? ImageId { get; set; }
+        [ForeignKey("ImageId")]
+        public Image Image { get; set; }
+    }
+}
